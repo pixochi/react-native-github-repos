79eb7eda4d8bddd89d4f1d371a87176199b8aa0c
Jakub Kozak <kozak.jakub55@gmail.com>
2021-05-15 20:16:43 +0200

    Move inline styles to StyleSheet in ListItem

--- components/GitHubRepos/ListItem.tsx
+++ components/GitHubRepos/ListItem.tsx
@@ -13,35 +13,28 @@ const ListItem: React.FC<{item: Types.GitHubRepo; onPress: () => void}> = ({
     <View style={styles.item}>
       <Text style={styles.title}>{item.name}</Text>
       <Text style={styles.description}>{item.description}</Text>
-      <View style={styles.statsContainer}>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            flex: 1,
-          }}>
-          <Text>{item.language}</Text>
-          <View style={{flexDirection: 'row'}}>
-            <View style={{flexDirection: 'row'}}>
-              <Text>{Helpers.normalizeCount(item.stargazers_count)}</Text>
-              <Icon
-                style={{marginLeft: 4}}
-                name="star"
-                size={16}
-                color="#f4b840"
-              />
-            </View>
-            <View style={{flexDirection: 'row'}}>
-              <Text style={{marginLeft: 20}}>
-                {Helpers.normalizeCount(item.forks_count)}
-              </Text>
-              <Icon
-                style={{marginLeft: 4}}
-                name="code-fork"
-                size={16}
-                color="#f4b840"
-              />
-            </View>
+      <View style={styles.infoContainer}>
+        <Text>{item.language}</Text>
+        <View style={styles.statsContainer}>
+          <View style={styles.countContainer}>
+            <Text>{Helpers.normalizeCount(item.stargazers_count)}</Text>
+            <Icon
+              style={styles.countIcon}
+              name="star"
+              size={16}
+              color="#f4b840"
+            />
+          </View>
+          <View style={styles.countContainer}>
+            <Text style={styles.forkCount}>
+              {Helpers.normalizeCount(item.forks_count)}
+            </Text>
+            <Icon
+              style={styles.countIcon}
+              name="code-fork"
+              size={16}
+              color="#f4b840"
+            />
           </View>
         </View>
       </View>
@@ -63,11 +56,17 @@ const styles = StyleSheet.create({
     fontSize: 32,
   },
   description: {paddingTop: 4},
-  statsContainer: {
+  infoContainer: {
+    flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between',
     marginTop: 16,
   },
+  statsContainer: {flexDirection: 'row'},
+  countContainer: {flexDirection: 'row'},
+  countIcon: {marginLeft: 4},
+  forkCount: {marginLeft: 20},
 });
 
 export default ListItem;
